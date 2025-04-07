a5147c180b48487106b481fe1e9edf0f5afe00c9
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-07 06:33:46 +0000

    Update App with Proper Routing Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import { ClerkProvider } from '@clerk/clerk-react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -24,7 +24,11 @@ function App() {
       afterSignUpUrl={clerkAfterSignUpUrl}
     >
       <BrowserRouter>
-        {/* Your app content */}
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/sign-in/*" element={<div>Sign In</div>} />
+          <Route path="/sign-up/*" element={<div>Sign Up</div>} />
+        </Routes>
       </BrowserRouter>
     </ClerkProvider>
   )
